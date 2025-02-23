9c35140a4cf09512903f759561affcd59acd06f7
honor <honor864510@gmail.com>
2025-02-23 22:15:56 +0500

    fix

--- lib/src/data_source/sync_local_data_source.dart
+++ lib/src/data_source/sync_local_data_source.dart
@@ -1,17 +1,15 @@
 import 'package:parse_server_sdk/parse_server_sdk.dart';
-import '../entity/sync_entity.dart';
+import 'package:parse_sync/src/entity/sync_entity.dart';
 import 'package:sembast/sembast.dart';
 
 class SyncLocalDataSource<T extends ParseObject> {
-  final Database database;
-  final StoreRef<String, Map<String, dynamic>> _store;
-  final T Function(Map<String, dynamic>) _fromJson;
-
   SyncLocalDataSource(
     this.database,
     String storeName,
-    this._fromJson,
   ) : _store = StoreRef<String, Map<String, dynamic>>(storeName);
+
+  final Database database;
+  final StoreRef<String, Map<String, dynamic>> _store;
 
   Future<void> putEntity(SyncEntity<T> entity) async {
     await _store.record(entity.objectId).put(database, entity.toMap());
@@ -22,14 +20,15 @@ class SyncLocalDataSource<T extends ParseObject> {
   }
 
   Stream<List<SyncEntity<T>>> watchAll() {
-    return _store
-        .query()
-        .onSnapshots(database)
-        .map((records) => records.map((r) => SyncEntity.fromMap(r.value, _fromJson)).toList());
+    return _store.query().onSnapshots(database).map((records) => records
+        .map(
+          (r) => SyncEntity<T>.fromMap(r.value),
+        )
+        .toList());
   }
 
   Future<SyncEntity<T>?> getEntity(String objectId) async {
     final record = await _store.record(objectId).get(database);
-    return record != null ? SyncEntity.fromMap(record, _fromJson) : null;
+    return record != null ? SyncEntity.fromMap(record) : null;
   }
 }
